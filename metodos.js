c0ec246e2cd3b0629cd0c1cbcf46769f1809afba
dudapascoall <142435751+dudapascoall@users.noreply.github.com>
2024-04-25 12:17:11 -0300

    Add files via upload

---
+++ metodos.js
@@ -0,0 +1,29 @@
+class giovani{
+    constructor(horario){
+        this.horarioAtual = horario;
+    }
+
+    queHorasSao(){
+        return this.horarioAtual;
+    }
+}
+
+class nadyeeska{
+    constructor(hoje){
+        this.hojeAtual = hoje;
+    }
+
+    queDiaEHoje(){
+        return this.hojeAtual;
+    }
+}
+
+class bernardo{
+    constructor(temperatura){
+        this.temperaturaAtual = temperatura;
+    }
+
+    qualATemperatura(){
+        return this.temperaturaAtual;
+    }
+}
